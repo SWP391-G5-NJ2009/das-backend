ab0f64d1e01b18f9fa5b7f8f39979ec2ecbe8c03
Khanh Vu <khanhvunam@gmail.com>
2026-06-10 00:14:49 +0700

    feat: add payment api

--- src/app.js
+++ src/app.js
@@ -3,19 +3,23 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const authRoutes = require("./routes/auth.routes");
+const paymentRoutes = require("./routes/payment.routes");
 const { errorMiddleware, notFoundMiddleware } = require("./middlewares/error.middleware");
 const { sendSuccess } = require("./utils/response");
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/api/health", (req, res) =>
   sendSuccess(res, 200, { status: "ok" }, "OK"),
 );
 
 app.use("/api/auth", authRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
