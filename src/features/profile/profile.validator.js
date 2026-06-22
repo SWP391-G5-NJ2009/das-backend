f19f1ccf121e442e0f2542e983026502f5f657d5
Nguyen Minh Truong <truongtkls8@gmail.com>
2026-06-22 22:39:54 +0700

    update: gender validation

--- src/features/profile/profile.validator.js
+++ src/features/profile/profile.validator.js
@@ -6,7 +6,7 @@ const updateMyProfileSchema = Joi.object({
   email: Joi.string().trim().email().allow("", null),
   phone: Joi.string().trim().min(8).max(20).allow("", null),
   birthDate: Joi.date().iso().allow("", null),
-  gender: Joi.string().trim().max(20).allow("", null),
+  gender: Joi.string().trim().valid("Male", "Female", "").allow(null),
   address: Joi.string().trim().max(255).allow("", null),
   speciality: Joi.string().trim().max(100).allow("", null),
 }).min(1);
