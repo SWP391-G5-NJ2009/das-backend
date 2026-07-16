730f22328ccfe271ab964657b191148b3a6657ba
duchuydt17 <88969009+duchuydt17@users.noreply.github.com>
2026-07-16 07:56:40 +0700

    fix: crash

--- src/features/clinicScheduleManagement/clinicScheduleManagement.controller.js
+++ src/features/clinicScheduleManagement/clinicScheduleManagement.controller.js
@@ -137,6 +137,16 @@ async function getMinEffectiveDate(req, res, next) {
     }
 }
 
+async function createVersionWithHours(req, res, next) {
+    try {
+        const { name, effectiveDate, hours } = req.body;
+        const data = await clinicScheduleManagementService.createVersionWithHours(name, effectiveDate, hours);
+        return sendSuccess(res, 201, data, "Tạo phiên bản với giờ làm việc thành công.");
+    } catch (err) {
+        return next(err);
+    }
+}
+
 module.exports = {
     getWorkingHour,
     updateWorkingHours,
